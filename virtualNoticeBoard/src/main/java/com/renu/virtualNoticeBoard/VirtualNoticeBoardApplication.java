562d00dd394732f1c2829da2b4a191b5b6539e41
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-07-13 23:27:12 -0800

    update has been completed successfully

---
+++ virtualNoticeBoard/src/main/java/com/renu/virtualNoticeBoard/VirtualNoticeBoardApplication.java
@@ -0,0 +1,12 @@
+package com.renu.virtualNoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualNoticeBoardApplication.class, args);
+	}
+}
