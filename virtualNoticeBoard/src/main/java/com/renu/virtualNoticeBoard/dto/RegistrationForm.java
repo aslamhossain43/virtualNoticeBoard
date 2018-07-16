f6e47fa30fbfd09e84bcede8f496ff809fde6742
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-07-15 22:49:05 -0800

    Update has been completed successfully

---
+++ virtualNoticeBoard/src/main/java/com/renu/virtualNoticeBoard/dto/RegistrationForm.java
@@ -0,0 +1,28 @@
+package com.renu.virtualNoticeBoard.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class RegistrationForm {
+
+	@NotEmpty
+	private String boardName;
+	@Size(min=4,max=180)
+	private String password;
+	public String getBoardName() {
+		return boardName;
+	}
+	public void setBoardName(String boardName) {
+		this.boardName = boardName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+}
